07a593e5e3d8cd88bf52a3dc61fc97e0c4382c5f
root <root@metonnomici.lapro.local>
2026-04-27 14:44:48 +0200

    fix db connection

--- websocket-server/server.js
+++ websocket-server/server.js
@@ -9,9 +9,9 @@ app.use(express.static('Public'));
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "Murmure",
-    password: "",
+    host: "127.0.0.1",
+    user: "root",
+    password: "3006",
     database: "Murmure"
 });
 
